a67b722ba4caa015532e64cba5aafdb0d5afaada
Artem <artchzhen@gmail.com>
2023-04-11 20:09:26 +0900

    added action button and other features

--- Milestone-project-4-5/Milestone-project-4-5/ViewController.swift
+++ Milestone-project-4-5/Milestone-project-4-5/ViewController.swift
@@ -8,31 +8,55 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
-    var shopingList = [String]()
-  
+    
+    var shoppingList = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Shoping list"
+        title = "Shopping list"
         view.backgroundColor = .cyan
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(added))
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tap))
+        
     }
-    @objc func added() {
-        let ac = UIAlertController(title: "Add products", message: nil, preferredStyle: .alert)
-        ac.textFields
+    
+    @objc func tap() {
+        let ac = UIAlertController(title: "Write items", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) {
+            [weak self, weak ac] action in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.added(item: item)
+        }
+        
+        ac.addAction(addAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(ac, animated: true)
+    }
+    
+    func added(item: String) {
+        
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shopingList.count
+        return shoppingList.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
-        cell.textLabel?.text = shopingList[indexPath.row]
+        cell.textLabel?.text = shoppingList[indexPath.row]
         return cell
     }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            shoppingList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
-
