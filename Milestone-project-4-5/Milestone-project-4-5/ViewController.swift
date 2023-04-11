e6474b37fe27bbab5383b1202c1cfc717c6dcbc3
Artem <artchzhen@gmail.com>
2023-04-11 11:15:49 +0900

    added button and title

--- Milestone-project-4-5/Milestone-project-4-5/ViewController.swift
+++ Milestone-project-4-5/Milestone-project-4-5/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var shopingList = [String]()
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Shoping list"
+        view.backgroundColor = .cyan
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(added))
+
     }
-
-
+    @objc func added() {
+        let ac = UIAlertController(title: "Add products", message: nil, preferredStyle: .alert)
+        ac.textFields
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shopingList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = shopingList[indexPath.row]
+        return cell
+    }
 }
 
